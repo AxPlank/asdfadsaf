da9a9f208288b2d947f07f4a87f27b45b8927d14
Papus <foal0912@gmail.com>
2022-09-11 23:45:05 +0900

    code refactoring, add comment and worte markdown

--- leaguetable/app.js
+++ leaguetable/app.js
@@ -8,20 +8,18 @@ const app = express();
 Setting
 */
 
-app.locals.pretty = true;
+app.locals.pretty = true; // 소스 코드 예쁘게 정렬
 
 // listen
 const port = 3000;
-app.listen(port, () => {
-    console.log('Connected!');
-});
+app.listen(port);
 
 // set
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.set('views', './views'); // View에 사용할 파일들을 위치시킬 폴더 지정
+app.set('view engine', 'pug'); // View에 사용할 언어 지정
 
 // use
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({ // 요청이 왔을 때, JSON 파일을 읽기 위한 설정
     extended: false
 }));
 
@@ -31,38 +29,17 @@ REST API
 
 // get
 
-app.get('/add', (req, res) => {
-    fs.readdir('data', (err, files) =>{
-        if (err) {
+app.get('/add', (req, res) => { // 리그 순위를 만드는 화면
+    fs.readdir('data', (err, files) =>{ // 파일이 위치한 폴더에 접속
+        if (err) { // 에러 발생 시 출력
             console.log(err);
             res.status(500).send("Internal Server Error!");
         }
+        const jsons = { // View에서 사용할 데이터
+            leagues: files
+        };
 
-        let id = req.params.id;
-
-        if (id) {
-            fs.readFile('data/'+id, 'utf-8', (err, data) => {
-                if (err) {
-                    console.log(err);
-                    res.status(500).send("Internal Server Error!");
-                }
-
-                const league_name = id.replace(/_/g, ' ');
-
-                const jsons = {
-                    leagues: files,
-                    teams: data.split(','),
-                    league_name: league_name
-                };
-
-                res.render('table', jsons);
-            });
-        } else {
-            const jsons = {
-                leagues: files,
-            };
-            res.render('add', jsons);
-        }
+        res.render('add', jsons);
     });
 });
 
@@ -73,27 +50,27 @@ app.get(['/', '/:id'], (req, res) => {
             res.status(500).send("Internal Server Error!");
         }
 
-        let id = req.params.id;
+        let id = req.params.id; // 특정 리그 순위표를 볼 때 사용
 
         if (id) {
             fs.readFile('data/'+id, 'utf-8', (err, data) => {
-                if (err) {
+                if (err) { // 에러 발생 시 출력
                     console.log(err);
                     res.status(500).send("Internal Server Error!");
                 }
 
-                const league_name = id.replace(/_/g, ' ');
+                const league_name = id.replace(/_/g, ' '); // 리그 순위표를 저장할 때, 리그 이름의 공백을 _로 대체했었음. 이를 다시 공백으로 대체하는 작업
 
-                const jsons = {
+                const jsons = { // View에서 사용할 데이터
                     leagues: files,
-                    teams: data.split(','),
+                    teams: data.split(','), // 보다 편리하게 사용하도록 데이터를 Array로 변환함
                     league_name: league_name
                 };
 
-                res.render('table', jsons);
+                res.render('table', jsons); // table:
             });
         } else {
-            const jsons = {
+            const jsons = { // View에서 사용할 데이터
                 leagues: files,
             };
             res.render('table', jsons);
@@ -102,30 +79,30 @@ app.get(['/', '/:id'], (req, res) => {
 });
 
 // post
-app.post('/', (req, res) => {
+app.post('/', (req, res) => { // 리그 순위를 저장할 때 사용
     // 변수에 전송된 데이터 저장
     let title = String(req.body.league).replace(/ /g, '_');
     let body = req.body;
     let keys = Object.keys(body);
 
-    let leagueData = Array();
+    let leagueData = String();
 
-    // 데이터 가공
-    for (let i = 1; i < 21; i++) { // value가 ''라는 것은 해당 리그의 리그 순위표가 모두 나왔다는 것과 동일한 의미를 가지므로 반복문 중단 선언
-        if (body[keys[i]] === '') {
+    // 데이터 가공. 리그 순위에 맞춰 팀들을 문자열의 형태로 저장
+    for (let i = 1; i < 21; i++) {
+        if (body[keys[i]] === '') { // 순위표의 순위에 해당하는 값이 ''라는 것은 모든 팀의 순위가 저장되었다는 의미함
             break;
         }
 
-        leagueData.push(body[keys[i]]);
+        leagueData += body[keys[i]] + ',';
     }
 
-    leagueData = leagueData.join(',');
+    leagueData = leagueData.substring(0, leagueData.length-1); // 맨 마지막 쉼표 제거
 
-    fs.writeFile('./data/'+title, leagueData, (err) => {
-        if (err) {
+    fs.writeFile('./data/'+title, leagueData, (err) => { // 데이터 저장
+        if (err) { // 에러 발생 시 에러 출력
             throw err;
         }
         
-        res.redirect('/');
+        res.redirect('/'); // 저장 후 '/'로 이동 (여기서는 메인페이지)
     });
 });
--- leaguetable/leaguetable.md
+++ leaguetable/leaguetable.md
@@ -1,29 +1,34 @@
 # Project: 5대 축구 리그 순위표
+<br>
 
 # Introduction
 ##### JavaScript는 웹에서의 다양한 동작을 구현하는 프론트엔드의 역할 뿐 아니라, 데이터베이스를 연동하고, API를 구현하는 백엔드의 역할도 할 수 있는 언어입니다. 그래서 저는 인프런 강의에서 배운 것을 토대로 JavaScript를 활용한 간단한 웹 서버를 제작해 보기로 하였습니다.
+<br>
 
 # Language and Framework
 <img src="https://img.shields.io/badge/Node.js_18.3.0-339933?style=for-the-badge&logo=Node.js&logoColor=FFFFFF">&nbsp;
 <img src="https://img.shields.io/badge/JavaScript_ES6-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=000000">&nbsp;
 <img src="https://img.shields.io/badge/express_4.18.1-000000?style=for-the-badge&logo=express&logoColor=FFFFFF">&nbsp;
 <img src="https://img.shields.io/badge/pug_3.0.2-A86454?style=for-the-badge&logo=pug&logoColor=000000">
+<br>
 
 # UI
-<img src='createtable.png'/><br>
+<img src='UI.png'/>
+<br>
 
 # Features
 ## 리그 순위표 추가
 ---
 ##### 리그와 해당 리그의 순위표를 추가할 수 있는 기능입니다.
----
-### Detail
----
+##### 리그 이름, 리그 순위표를 입력하고 제출을 하면 리그 순위표가 파일의 형태로 저장됩니다.
+##### 파일의 제목이 같을 경우, 입력했을 때 해당 파일을 덮어쓰기 떄문에 순위표를 수정하는 기능도 할 수 있습니다.
+<br>
 
-##### 해당 화면에서 리그 이름, 리그 순위표를 입력하고 제출을 하면 리그 순위표가 파일의 형태로 저장됩니다.
 
 ## 리그 순위표 검색
 ---
-##### 리그를 클릭했을 때, 해당되는 리그의 순위표를 볼 수 있는 기능입니다. 
-### Detail
----
+##### 리그를 클릭했을 때, 해당되는 리그의 순위표를 볼 수 있는 기능입니다.
+<br>
+
+# Feedback
+##### 
