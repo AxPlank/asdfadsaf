bbbaa9b5aabae729a624e090b5a4bc9b1700a156
Papus <foal0912@gmail.com>
2022-11-11 13:55:25 +0900

    installed multer

--- practice/app.js
+++ practice/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const router = express.Router()
 const bodyParser = require('body-parser');
 
 const db = require('mysql').createConnection({
@@ -13,12 +14,26 @@ const db = require('mysql').createConnection({
 app.locals.pretty = true;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('static'));
+app.use('/', router);
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.listen(3003, () => {
     console.log(`Connected Port 3003
     http://localhost:3003/`);
 });
 
-app.get('/', (req, res) => {
-    res.send("HELLO");
+router.get('/', (req, res) => {
+    res.render('main');
+});
+
+router.get('/:type', (req, res) => {
+    const obj = {
+        type: req.params.type
+    };
+
+    res.render('formm', obj);
+}).post('/:type', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
