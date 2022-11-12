0b0f4585cf1073359be5981e44744c66747e76af
Papus <foal0912@gmail.com>
2022-11-12 23:18:35 +0900

    multer (1)

--- practice/app.js
+++ practice/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router()
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const db = require('mysql').createConnection({
     host: 'localhost',
@@ -10,6 +11,14 @@ const db = require('mysql').createConnection({
     port: 3306,
     database: 'thirdproject'
 });
+
+// const upload = multer({
+//     dest: './uploads/'
+// });
+
+const upload = multer({ // dest: 저장 경로, storage를 이용할 경우 좀 더 자세하게 사용가능
+    dest: './uploads/'
+}).array('image', 10);
 
 app.locals.pretty = true;
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,13 +36,18 @@ router.get('/', (req, res) => {
     res.render('main');
 });
 
-router.get('/:type', (req, res) => {
+router.get('/image', (req, res) => {
     const obj = {
         type: req.params.type
     };
 
     res.render('formm', obj);
-}).post('/:type', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+}).post('/image', (req, res) => {
+    upload(req, res, (err) => {
+        if(err) {
+            res.send(`<a href='/'>Back</a>`);
+        } else {
+            res.send(req.files);
+        }
+    });
 });
